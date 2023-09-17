6a415894411069a6ca5271ab6a2c7376552f1f98
DakshGopani36 <86640269+dakshgopani@users.noreply.github.com>
2023-09-17 23:31:51 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,43 @@
+//console.log("This is my script");
+
+let result = {
+  tag: "",
+  free: true,
+  role: false,
+  user: "dakshcubesquare123",
+  email: "dakshcubesquare123@gmail.com",
+  score: 0.64,
+  state: "deliverable",
+  domain: "gmail.com",
+  reason: "valid_mailbox",
+  mx_found: true,
+  catch_all: null,
+  disposable: false,
+  smtp_check: true,
+  did_you_mean: "",
+  format_valid: true,
+};
+
+SubmitBTN.addEventListener("click", async (e) => {
+  e.preventDefault()
+  console.log("clicked");
+  resultsCONTAINER.innerHTML= `<img width="100px" src="loadingSVG.svg" alt=""></img>`
+  let key = "ema_live_jXQyeZqxsKc9yNsVSBbVViZkRCW1PUplUmMCTvLY";
+  let email = document.getElementById("username").value;
+
+  let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`;
+
+  let res=await fetch(url)
+  let result=await res.json()
+
+  let str = ``;
+  for (key of Object.keys(result)) {
+    if(result[key]!=="" && result[key]!== " "){
+      str = str + `<div>${key}: ${result[key]}</div>`;
+    }
+    
+  }
+  console.log(str);
+  resultsCONTAINER.innerHTML = str;
+
+});
